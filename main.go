76d12e79a62421f88d3b0d9f452b49472d34400c
Jiharev Zahar <jiharevzahar@gmail.com>
2020-04-10 12:49:32 +0300

    Median(arr must be odd)

--- main.go
+++ main.go
@@ -2,6 +2,33 @@ package main
 
 import "fmt"
 
+func median(arr [5]int) {
+	size := len(arr)
+	for i := 0; i < size; i++ {
+		var (
+			n int = 0
+			m int = 0
+		)
+		var j = 0
+		for ; j < size; j++ {
+			if arr[j] != arr[i] {
+				if arr[j] > arr[i] {
+					n++
+				} else {
+					m++
+				}
+			}
+		}
+		if n == m {
+			fmt.Println("median - ", arr[i])
+		}
+	}
+}
+
 func main() {
-	fmt.Println("Hello world")
+
+	var numbers [5]int = [5]int{1, 2, 3, -5, 5}
+	fmt.Println("res")
+	median(numbers)
+
 }
