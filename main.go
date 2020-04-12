7967bb85b0b1f78bf11823ebf05e4039dfbc1ef8
Jiharev Zahar <jiharevzahar@gmail.com>
2020-04-12 17:48:03 +0300

    Added return

--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 
 import "fmt"
 
-func median(arr [5]int) {
+//
+//In my case it's not nessesary to return float64 due to the fact that i select the median from the array.
+//If it was meant to find the median(not from the array), then the implementation would be much simplier
+//
+
+func median(arr [5]int) float64 {
 	size := len(arr)
 	for i := 0; i < size; i++ {
 		var (
@@ -20,15 +25,13 @@ func median(arr [5]int) {
 			}
 		}
 		if n == m {
-			fmt.Println("median - ", arr[i])
+			return float64(arr[i])
 		}
 	}
+	return 0
 }
 
 func main() {
-
 	var numbers [5]int = [5]int{1, 2, 3, -5, 5}
-	fmt.Println("res")
-	median(numbers)
-
+	fmt.Println(median(numbers))
 }
