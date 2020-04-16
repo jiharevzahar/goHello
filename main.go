571a6165384b9aa6e3436c6ec40d3e2a73f5a21c
jiharevzahar <61798710+jiharevzahar@users.noreply.github.com>
2020-04-16 12:40:21 +0300

    Median

--- main.go
+++ main.go
@@ -2,36 +2,33 @@ package main
 
 import "fmt"
 
-//
-//In my case it's not nessesary to return float64 due to the fact that i select the median from the array.
-//If it was meant to find the median(not from the array), then the implementation would be much simplier
-//
-
 func median(arr [5]int) float64 {
-	size := len(arr)
-	for i := 0; i < size; i++ {
-		var (
-			n int = 0
-			m int = 0
-		)
-		var j = 0
-		for ; j < size; j++ {
-			if arr[j] != arr[i] {
-				if arr[j] > arr[i] {
-					n++
-				} else {
-					m++
-				}
+	n := len(arr)
+	var i, j, a int
+
+	for i = 0; i < n; i++ {
+		for j = i + 1; j < n; j++ {
+			if arr[i] > arr[j] {
+				a = arr[i]
+				arr[i] = arr[j]
+				arr[j] = a
+			}
+			if arr[i] > arr[j] {
+				a = arr[i]
+				arr[i] = arr[j]
+				arr[j] = a
 			}
 		}
-		if n == m {
-			return float64(arr[i])
-		}
 	}
-	return 0
+
+	if n%2 == 0 {
+		return (float64(arr[n/2]) + float64(arr[n/2-1])) / 2.0
+	} else {
+		return float64(arr[n/2])
+	}
 }
 
 func main() {
-	var numbers [5]int = [5]int{1, 2, 3, -5, 5}
+	var numbers [5]int = [5]int{1, 2, 3, 25, 50}
 	fmt.Println(median(numbers))
 }
