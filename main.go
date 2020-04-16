1e358c8f8af26029bcc92d1ff1889226deb3f755
jiharevzahar <61798710+jiharevzahar@users.noreply.github.com>
2020-04-16 15:49:27 +0300

    fixed sort

--- main.go
+++ main.go
@@ -1,25 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
-func median(arr [5]int) float64 {
+func median(arr []int) float64 {
 	n := len(arr)
-	var i, j, a int
 
-	for i = 0; i < n; i++ {
-		for j = i + 1; j < n; j++ {
-			if arr[i] > arr[j] {
-				a = arr[i]
-				arr[i] = arr[j]
-				arr[j] = a
-			}
-			if arr[i] > arr[j] {
-				a = arr[i]
-				arr[i] = arr[j]
-				arr[j] = a
-			}
-		}
-	}
+	sort.Ints(arr)
 
 	if n%2 == 0 {
 		return (float64(arr[n/2]) + float64(arr[n/2-1])) / 2.0
@@ -29,6 +18,6 @@ func median(arr [5]int) float64 {
 }
 
 func main() {
-	var numbers [5]int = [5]int{1, 2, 3, 25, 50}
+	var numbers []int = []int{1, 2, 3, 25, 50}
 	fmt.Println(median(numbers))
 }
