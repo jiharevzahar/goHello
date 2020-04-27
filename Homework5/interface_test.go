1d676780225e42158de649e299f2d6be9e858f7e
jiharevzahar <61798710+jiharevzahar@users.noreply.github.com>
2020-04-27 14:15:03 +0300

    Update interface_test.go

--- Homework5/interface_test.go
+++ Homework5/interface_test.go
@@ -6,28 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-/*func TestArea(t *testing.T) {
-
-	checkArea := func(t *testing.T, figure Figure, want float64) {
-		t.Helper()
-		got := figure.Area()
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	}
-
-	t.Run("squares", func(t *testing.T) {
-		square := Square{5}
-		checkArea(t, square, 25.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
-
-}*/
-
 func TestArea(t *testing.T) {
 
 	checkArea := func(t *testing.T, figure Figure, want float64) {
