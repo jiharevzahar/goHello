531b144fc73073d08c548334be4ce502f73df9e3
jiharevzahar <61798710+jiharevzahar@users.noreply.github.com>
2020-04-27 13:37:55 +0300

    Redid according to the condition of the task

--- task_fourth/Sorting.go
+++ task_fourth/Sorting.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"sort"
 	"time"
-	"unicode/utf8"
 )
 
 type Person struct {
@@ -15,46 +14,31 @@ type Person struct {
 
 type People []Person
 
-func (a People) Len() int {
-	return len(a)
+func (p People) Len() int {
+	return len(p)
 }
-func (a People) Less(i, j int) bool {
-	iRune, _ := utf8.DecodeRuneInString(a[i].firstName)
-	jRune, _ := utf8.DecodeRuneInString(a[j].firstName)
 
-	zRune, _ := utf8.DecodeRuneInString(a[i].lastName)
-	xRune, _ := utf8.DecodeRuneInString(a[j].lastName)
+func (p People) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
 
-	if int32(iRune) == int32(jRune) {
-		if int32(zRune) == int32(xRune) {
-			return a[i].birthday.Before(a[j].birthday)
-		} else {
-			return int32(zRune) < int32(xRune)
+func (p People) Less(i, j int) bool {
+	if p[i].birthday.Unix() == p[j].birthday.Unix() {
+		if p[i].firstName == p[j].firstName {
+			return p[i].lastName < p[j].lastName
 		}
-	} else {
-		return int32(iRune) < int32(jRune)
+		return p[i].firstName < p[j].firstName
 	}
+	return p[i].birthday.Unix() > p[j].birthday.Unix()
 }
-func (a People) Swap(i, j int) {
-	a[i], a[j] = a[j], a[i]
-}
+
 func main() {
-	groupA := []Person{
-		{"Clara", "Jonson", time.Now()}, {"Bndr", "Taiwon", time.Date(2000, 0, 1, 12, 0, 0, 0, time.UTC)}, {"Abc", "cvx", time.Date(1999, 0, 1, 12, 0, 0, 0, time.UTC)},
-	}
 
-	sort.Sort(People(groupA))
-	fmt.Println(groupA)
-
-	groupB := []Person{
-		{"AAA", "CCC", time.Now()}, {"AAA", "BBB", time.Date(2000, 0, 1, 12, 0, 0, 0, time.UTC)}, {"AAA", "AAA", time.Date(1999, 0, 1, 12, 0, 0, 0, time.UTC)},
+	groupB := People{
+		{"a", "c", time.Date(2000, 0, 1, 12, 0, 0, 0, time.UTC)},
+		{"a", "b", time.Date(2000, 0, 1, 12, 0, 0, 0, time.UTC)},
+		{"a", "a", time.Now()},
 	}
-	sort.Sort(People(groupB))
+	sort.Sort(groupB)
 	fmt.Println(groupB)
-
-	groupC := []Person{
-		{"BBB", "BBB", time.Now()}, {"AAA", "AAA", time.Date(2005, 0, 1, 12, 0, 0, 0, time.UTC)}, {"BBB", "BBB", time.Date(1999, 0, 1, 12, 0, 0, 0, time.UTC)},
-	}
-	sort.Sort(People(groupC))
-	fmt.Println(groupC)
 }
