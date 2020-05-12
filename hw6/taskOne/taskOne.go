70f3ab686af39831554899480d295862e2c02014
jiharevzahar <61798710+jiharevzahar@users.noreply.github.com>
2020-05-12 18:22:23 +0300

    fixed

--- hw6/taskOne/taskOne.go
+++ hw6/taskOne/taskOne.go
@@ -7,39 +7,31 @@ import (
 )
 
 type request struct {
-	Host       string  `json:"host"`
-	UserAgent  string  `json:"user_agent"`
-	RequestURL string  `json:"request_uri"`
-	Headers    headers `json:"headers"`
+	Host       string              `json:"host"`
+	UserAgent  string              `json:"user_agent"`
+	RequestURL string              `json:"request_uri"`
+	Header     map[string][]string `json:"headers"`
 }
 
-type headers struct {
-	Accept    []string `json:"Accept"`
-	UserAgent []string `json:"User-Agent"`
-}
+func jsonOutput(w http.ResponseWriter, r *http.Request) {
+	var outputRequest request
+	outputRequest.Host = r.Host
+	outputRequest.UserAgent = r.UserAgent()
+	outputRequest.RequestURL = r.RequestURI
+	outputRequest.Header = r.Header
 
-func main() {
-	req := &request{
-		Host:       "127.0.0.1:8080",
-		UserAgent:  "curl/7.67.0",
-		RequestURL: "/anything/you?want",
-		Headers: headers{
-			Accept:    []string{"*/*"},
-			UserAgent: []string{"curl/7.67.0"},
-		},
-	}
-
-	js, err := json.Marshal(*req)
+	output, err := json.Marshal(outputRequest)
 	if err != nil {
 		log.Panic(err)
 	}
+	w.Write(output)
+}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write(js)
-	})
+func main() {
+
+	http.HandleFunc("/", jsonOutput)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Panic(err)
 	}
 }
-
